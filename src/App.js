9d6fb75a5c86fbfdd71dd85fdfd744f15f2bf0dc
gingercook <vahitaras@gmail.com>
2022-03-26 20:49:39 -0400

    experimenting with columns feature of tailwind css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 function App() {
   return (
     <>
-      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg">
+      <div class="p-6 max-w-sm mx-auto bg-white dark:bg-black rounded-xl shadow-lg">
         <div class="text-center">
           <div class="text-xl font-medium text-black">ChitChat</div>
           <p class="text-slate-500">You have a new message!</p>
           <button class="bg-sky-600 hover:bg-sky-700 ...">Hit</button>
         </div>
+      </div>
+
+      <div class="mx-auto text-center columns-3xs">
+        <p>Aras</p>
+        <p>Aras</p>
+        <p>Aras</p>
+        <p>Aras</p>
+        <p>Aras</p>
+        <p>Aras</p>
       </div>
     </>
   );
