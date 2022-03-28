8a4649402ccbc2960f45a0c8c91576871fa0aad7
gingercook <vahitaras@gmail.com>
2022-03-27 23:04:24 -0400

    tailwind playground

--- src/App.js
+++ src/App.js
@@ -11,13 +11,32 @@ function App() {
         </div>
       </div>
 
-      <div class="mx-auto text-center columns-3xs">
+      <div class="mx-auto text-center columns-1 md:columns-3 gap-2 hover:columns-2">
         <p>Aras</p>
         <p>Aras</p>
         <p>Aras</p>
         <p>Aras</p>
         <p>Aras</p>
         <p>Aras</p>
+      </div>
+
+      <div class="mx-auto max-w-md text-center p-12">
+        <p>Maybe we can live without...</p>
+        <p class="break-after-column">Sure, go ahead, laugh...</p>
+        <p class="break-inside-avoid-column">Maybe we can live without...Maybe we can live without...Maybe we can live without...Maybe we can live without...</p>
+      </div>
+
+      <div class="bg-yellow-300 w-32 text-center mx-auto rounded-xl">
+        <p class="p-6 box-border">Aras Aras Aras Aras Aras Aras Aras</p>
+        <p class="p-6 box-border">Aras Aras Aras Aras</p>
+      </div>
+
+      <div class="items-center h-20 max-auto mx-w-md bg-yellow-600">
+        <div>Aras</div>
+        <div>
+          <strong>Aras Sen</strong>
+          <span>Web Developer</span>
+        </div>
       </div>
     </>
   );
