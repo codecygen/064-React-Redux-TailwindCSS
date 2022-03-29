f465edddffd3301f5b351acfb66ce5495c4f4af4
gingercook <vahitaras@gmail.com>
2022-03-28 22:35:41 -0400

    working on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import { SiReactivex } from "react-icons/si";
+
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  );
+    return (
+        <section class="fixed w-screen">
+            <div class="flex flex-row relative mx-36 items-center">
+                <SiReactivex size="50" />
+                <div>Navbar</div>
+                <div class="absolute right-0">aras</div>
+            </div>
+        </section>
+    );
 };
 
 export default Navbar;
