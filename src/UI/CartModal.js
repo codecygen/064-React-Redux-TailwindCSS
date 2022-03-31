445c969a155a09fa47c7b7b970017b7d3e7a0621
gingercook <vahitaras@gmail.com>
2022-03-30 21:02:52 -0400

    modal overlay is worked on for cart card

--- src/UI/CartModal.js
+++ src/UI/CartModal.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 
 const CartBackdrop = () => {
     return (
-        <div>Backdrop</div>
+        <div class="fixed top-0 left-0 h-screen w-screen bg-[rgb(255,0,0,0.3)] z-10">
+            {/* Backdrop */}
+        </div>
     );
 };
 
 const CartOverlay = () => {
     return (
-        <div class="py-10">CartCard</div>
+        <div class="fixed top-1/3 left-1/3 h-1/3 w-1/3 bg-blue z-50 overflow-hidden text-center bg-red-300 rounded-md">
+            CartOverlay
+        </div>
     );
 };
 
