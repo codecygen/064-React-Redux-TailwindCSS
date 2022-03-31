1399293cde2bf8898fe86048975b47b98f360feb
gingercook <vahitaras@gmail.com>
2022-03-31 12:33:29 -0400

    shopping cart layout is almost done

--- src/UI/CartCard.js
+++ src/UI/CartCard.js
@@ -1,18 +1,33 @@
 import React from 'react'
 
+import { ImBin } from "react-icons/im";
+
 const CartCard = props => {
     return (
-        <div class="m-5 flex flex-row items-center bg-white rounded-xl">
-            <div class="h-32 w-1/3">
+        <div class="my-2 mx-16 flex flex-row bg-slate-100 rounded-xl shadow-xl">
+            <div class="h-32 w-2/6">
                 <img src={props.img} class="h-full mx-auto rounded-xl" />
             </div>
 
-            <div class="w-1/3 flex flex-row">
-                <p class="text-xl italic">{props.desc}</p>
-                <button>hey</button>
+            <div class="w-3/6 inline-flex justify-center items-center">
+                <p class="text-xl italic pr-8">{props.desc}</p>
+                <div class="flex flex-row items-center">
+                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+                        -
+                    </button>
+                    <p class="px-2 py-2 bg-slate-200">5</p>
+                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+                        +
+                    </button>
+                </div>
             </div>
 
-            <p class="p-5 pb-5 text-xl font-bold">${props.price}</p>
+            <div class="w-1/6 flex flex-col items-center justify-center">
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
+                <ImBin />
+                </button>
+                <p class="text-xl font-bold pt-10">${props.price}</p>
+            </div>
         </div>
     );
 };
--- src/UI/CartWindow.js
+++ src/UI/CartWindow.js
@@ -11,10 +11,15 @@ import vrHeadset from '../assets/images/vrHeadset.jpg';
 
 const CartWindow = () => {
     return (
-        <div class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-128 bg-blue z-50 text-center bg-red-300 rounded-md">
+        <div class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-128 bg-blue z-50 text-center bg-red-300 rounded-md shadow-2xl">
             <h1 class="text-3xl font-bold py-5">Shopping Cart</h1>
             <CartCard img={headset} desc="Headset" price="499" />
             <CartCard img={laptop} desc="Laptop" price="2439" />
+
+            <div class="flex flex-row justify-end">
+                <h1 class="text-2xl font-bold w-5/6">Subtotal</h1>
+                <h1 class="text-2xl font-bold w-1/6">2</h1>
+            </div>
         </div>
     );
 };
