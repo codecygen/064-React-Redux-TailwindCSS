644735db1e1c9dd8a6cdc6a037a9df26d8f8fe36
gingercook <vahitaras@gmail.com>
2022-04-09 23:45:48 -0400

    session storage logic is added to fetch data from database only the first time the app loads on a new tab

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ import CartModal from './UI/CartModal';
 import { useSelector } from 'react-redux';
 
 const App = () => {
+  const isItemFetched = sessionStorage.getItem('isItemFetched') ? true : false;
+
+  if (!sessionStorage.getItem('isItemFetched')) {
+    sessionStorage.setItem('isItemFetched', true);
+  }
+
+  if (!isItemFetched) {
+    console.log('Fetching Data!');
+  }
+
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
   const [isCartShown, setIsCartShown] = useState(false);
